fc34c2dfe3999d6e4f9ff5861aa8c66328cdf923
Mauricio Payetta <mauricio.payetta@dianrong.com>
2015-08-18 12:40:53 +0800

    added gulp-mocha and changed default test script in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,32 @@
 
 var gulp = require('gulp');
 var linter = require('gulp-jslint');
+var mocha = require('gulp-mocha');
+var handleMochaError = function (err) {
+    console.log('Mocha encountered an error, exiting with status 1');
+    console.log('Error:', err.message);
+    process.exit(1);
+};
 
-gulp.task('lint', function() {
+
+gulp.task('lint', function () {
     gulp.src(['lib/**/*.js', 'test/**/*.js'])
         .pipe(linter());
+});
+
+gulp.task('mocha', function () {
+    var mochaErr;
+    return gulp.src(['test/**/*.js'], {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+        .on('error', function (err) {
+            console.error('ERROR:', err.message);
+            console.error('Stack:', err.stack);
+            mochaErr = err;
+        })
+        .on('end', function () {
+            if (mochaErr) return handleMochaError(mochaErr);
+            // Force mocha to exit, because gulp-mocha is stupid.
+            process.exit();
+        });
+    ;
 });
